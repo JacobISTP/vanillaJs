c1f2011ecf3720868c369c3fe07cdeb272d985b1
jangbly62 <82589477+jangbly62@users.noreply.github.com>
2021-07-11 12:17:17 +0900

    img, clock

---
+++ js/clock.js
@@ -0,0 +1,11 @@
+const clock = document.querySelector("#clock");
+
+function timeNow() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`
+}
+timeNow();
+setInterval(timeNow, 1000);
